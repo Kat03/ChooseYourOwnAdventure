3ce212ea28a3f50017fd93508b2641e9e87c3d59
Kat03 <47905001+Kat03@users.noreply.github.com>
2020-02-09 19:31:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,114 @@
+function createStory(name){
+    var page = [ ];
+        
+    page[14] = `${name} turns right into a forest. \nAs they walk into the forest, ${name} hears an eerie sound. \n${name} keeps moving forward until they see a cave. \nEnter 5 if ${name} enters the cave or 7 if they continue moving forward.`;
+    page[6] = `The eerie noise gets louder and louder. \n${name} keeps moving forward and trips over a pumpkin. \n${name} falls into a hole and is unable to escape.\nNow ${name} has to deal with a VERY angry and VERY big spider. Enter 49 to fight or 38 to run`;
+    page[4] = `The cave has no light at all.\n${name} feels something rustle pass them.\n${name} tries not to make a sound, but ${name} recognizes the pathways and they seem familiar\n${name} runs into a doorway and see a human-like figure. Enter 1 to continue or 25 to leave and run for their life.`;
+    page[0] = `${name} moves forward, like they have done this before. On their way in, ${name} bumps into a ghost. Their head starts to spin and everything starts to change. Enter 51 to continue.`;
+    page[50] = `${name} has now become the ghost. Enjoy the rest of their time haunting people in this cave.`;
+    page[24] = `${name} runs for their life and makes it out of the forest, just in time for their favorite show to come on. Enter 11 to go home, or enter 9 to go to the other passageway.`;
+    page[10] = `${name} makes their way home, and is able to relax!`;
+    page[37] = `${name} has tried to run around it, but ${name} soon gets exhausted. As a last attempt, they try to scale the wall of the hole. Enter 45 to continue`;
+    page[44] = `${name} successfully scales the wall but now the spider can too. Enter 12 run forward or 17 to go back the way ${name} came.`;
+    page[16] = `The spider keeps pursuing them. As ${name} runs out of the forest, the spider follows them but gets burnt by the hot sunlight. ${name} is able to successfully go back home.`;
+    page[11] = `The spider keeps moving behind you.\nThe path turns right and ${name} follows it, but they are lead into a dead end. Enter 22 to continue.`;
+    page[21] = `The spider traps ${name} in its web, leaving ${name} to be its next meal. The End.`;
+    page[48] = `${name} aims at the spider's leg, and surprisingly, the spider falls. Enter 35 to keep moving forward or 21 to investigate the spider.`;
+    page[20] = `${name} sees a bundle tied on to the spider's leg. Enter 19 to open it.`;
+    page[18] = `${name} opens the bundle and see a rather large collection of berries. Enter 31 to leave them there or 16 to eat one.`;
+    page[30] = `${name} leaves the berries there, and calmly walk out of the forest and return home safely.`;
+    page[15] = `${name} pops a purple berry in their mouth and their whole body starts to swell.\n${name} slowly starts transforming into a very large spider, just like the one they fought. Enter 3 to continue.`;
+    page[2] = `${name} is now a spider. Enter 33 to look for a home and 23 to stay where they are.`;
+    page[32] = `${name} looks for a home and finds a rather large web nearby. ${name} decides to stay there comfortably, eating the occasional human traveler. Good-Bye.`;
+    page[22] = `${name} stays there but a traveler come running into the forest and strikes ${name} dead.'I've done this too many times' they say.`;
+    page[34] = `${name} moves forward at a considerably slower pace.\nThe path turns right. Enter 4 to follow the path or 23 to turn to the left instead.`;
+    page[3] = `${name} keeps following the path when ${name} sees the spider's web.\nEnter 10 to keep moving forward, or Enter 41 to back away.`;
+    page[9] = `${name} moves forward, but there is nothing else to discover. ${name} quickly retraces their steps and leave the forest relaxed`;
+    page[40] = `${name} backs away, but suddenly run into another web that wasn't there before. Enter 24 to continue.`;
+    page[23] = `${name} is stuck in the trap of webs. There is no way to escape a VERY hungry spider.`;
+    page[8] = `${name} heads towards a river. There are two bridges there. Enter 6 if ${name} goes on the fancy one, and 8 if they go on the rickety one`;
+    page[7] = `${name} is able to successfully cross the river. ${name} realizes that across the river is a castle. Enter 14 to continue.`;
+    page[13] = `${name} enters the castle and the doors lock behind them. Enter 44 to try and open them or 20 to explore the castle.`;
+    page[43] = `${name} tries to open the door but there is no way to open them. ${name} finds a coin on the ground. Enter 36 to pick it up or 39 to leave it there.`;
+    page[35] = `${name} picks up the coin and tosses it at the doors, which miraculously open. ${name} now goes home to play some actual video games.`;
+    page[38] = `${name} remains lost in the castle for eternity, never being able to escape. Have fun!`;
+    page[19] = `${name} walks right into a royal meeting between the King and his advisors. The king bows to them. Enter 27 to bow back, or 50 to run away.`;
+    page[49] = `${name} runs away, and slam right into the closed doors. ${name} is thrown into the moat, and left to walk home in disgrace.`;
+    page[26] = `${name} bowed back, which starts a duel. Enter 29 to throw their right shoe or 47 to throw their left shoe.`;
+    page[28] = `${name} is wearing a flip flop on this shoe. The king slices it in midair before slicing their head off.`;
+    page[46] = `${name} is wearing a metal-studded boot. It catches the king off guard and knocks him unconscious. Enter 32 to continue.`;
+    page[31] = `The advisors name ${name} king and then throw ${name} into prison for life, because they are the actual ones in power. Have fun!`;
+    page[5] = `The fanciness was a trap. ${name} is now thrown off the bridge and into the fast moving current. Enter 40 to continue.`;
+    page[39] = `${name} swims along the current until they reach a piece of land in the middle of the river. Enter 13 if ${name} sits there, and 43 if ${name} tries to avoid it.`;
+    page[12] = `${name} sit on the piece of land, catching their breath, but they are unable to move away. ${name} remains stuck on that little island.`;
+    page[42] =`${name} avoids the island but then crashes into a stone wall. There is a ladder nearby and ${name} climbs up it. Enter 18 to continue.`;
+    page[17] = `${name} climbs up the ladder and realize that there are houses nearby. Enter 26 if ${name} climbs down the ladder or Enter 46 if ${name} crosses the wall.`;
+    page[45] = `${name} crosses the wall and start approaching the village. Enter 30 to keep moving forward.`;
+    page[29] = `As they walk closer and closer to the houses, ${name} realize that they are very big. Enter 34 to run away or 37 to keep moving forward.`;
+    page[33] = `${name} runs into the stone wall, and instantly get a concussion. ${name} is unable to move any further.`;
+    page[36] = `The giants from inside the houses offer ${name} some cake that they have to eat. Enter 42 to continue.`;
+    page[41] = `${name} slowly grows, become the same size as the giants, where they live happily.`;
+    page[25] = `${name} climbs down the ladder and swims back to their starting point. Enter 28 to go home.`;
+    page[27] = `${name} head from the river to their house, except that is not their house. Enter 48 to continue.`;
+    page[47] = `${name} realizes that they are just dreaming. ${name} walks right back into their house and takes a nap.`;
+
+    return page;
+}
+
+function getName(){
+    const name = document.querySelector("#player-name").value;
+}
+
+
+
+
+/*function myFunction() { 
+  document.getElementById("r").innerHTML = ${page[0]};
+}*/
+var name;
+
+
+function setName(){
+    name = document.querySelector("#player-name").value;
+    console.log(name);        
+    document.getElementById("start-button").innerText = "Continue";
+    document.getElementById("player-name").disabled = true;
+    document.getElementById("start-button").disabled = true;
+    document.getElementById("l").disabled = false;
+    document.getElementById("choice").disabled = false;
+    setFirst("You are walking on a road when you reach a fork. Enter 15 if you turn right, and 9 if you turn left.");
+    
+}
+function setFirst(text){
+    console.log("debug");
+    document.getElementById("enterIn").innerHTML = text;
+}
+
+function runFile(){   
+    console.log("yeet");    
+    var choice = document.querySelector("#choice").value - 1;
+    page=createStory(name);
+    document.getElementById("enterIn").innerHTML = page[choice];
+    document.getElementById("choice").innerHTML = "";
+
+    if(choice === 16 || choice === 9 || choice === 30 || choice === 10 || choice === 47 || choice === 41 || choice === 35 || choice === 21 || choice === 22 || choice === 23 || choice === 50 || choice === 12 || choice === 33 || choice === 38 || choice === 31 || choice === 28 || choice === 32 || choice === 49) {
+        document.getElementById("choice").disabled = true;
+        if(choice === 16 || choice === 9 || choice === 30 || choice === 10 || choice === 47 || choice === 41 || choice === 35 ){
+            setTimeout(() => {
+                document.getElementById("enterIn").innerHTML = " ";
+                lucky.style.visibility = "visible";
+            }, 5000);
+        }
+        if(choice === 21 || choice === 22 || choice === 23 || choice === 50 || choice === 12 || choice === 33 || choice === 38 || choice === 31 || choice === 28){
+            setTimeout(() => {document.getElementById("enterIn").innerHTML = " "; unlucky.style.visibility = "visible";}, 5000);
+        }
+        if(choice === 32 || choice === 49){
+            setTimeout(() => {document.getElementById("enterIn").innerHTML = " ";
+            luckless.style.visibility = "visible";
+        }, 5000);
+        }        
+    }   
+    
+    
+}
+
